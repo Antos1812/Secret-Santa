3c306076c9383409535c7b9001e714931544e001
Antek <un.tek2008@gmail.com>
2024-12-11 16:08:48 +0100

    Reorganized code

--- logic/index.ts
+++ logic/index.ts
@@ -1,45 +1,33 @@
-
-
 // Participant
 interface Participant {
   name: string;
 }
-
 interface Assignment {
   receiver: Participant;
   giver: Participant;
 }
-
 // Group
 class Group {
   private participants: Participant[] = [];
   private assignments: Assignment[] = [];
 
-  constructor(name: string) {
-    name
-  }
-
+  constructor(name: string) {name}
   addParticipant(name: string): void {
-    // TODO: Dodać walidację czy ju taki nie istnieje w grupie
     if (!this.ParticipantExist(name)) {
       this.participants.push({ name });
     }
   }
-
   private ParticipantExist(name: string): boolean {
     return this.participants.some(p => p.name === name);
   }
-
   draw() {
     const shuffla = [...this.participants].sort(() => Math.random() - 0.5);
-
     for (let i = 0; i < shuffla.length; i++) {
       const giver = shuffla[i];
       const receiver = shuffla[(i + 1) % shuffla.length]; // ostatni dostaje pierwszego
       this.assignments.push({ giver, receiver });
     }
   }
-
   getParticipants(): Participant[] {
     return this.participants
   }
@@ -51,7 +39,6 @@ class Group {
 //Manager
 class SecretSantaManager {
   groups: Map<string, Group> = new Map();  // kolekcja grup;
-
   createGroup(name: string) {
     if (!this.groups.has(name)) {
       this.groups.set(name, new Group(name));
@@ -59,7 +46,6 @@ class SecretSantaManager {
   }
   addParticipantToGroup(groupName: string, participantName: string) {
     const group = this.groups.get(groupName);
-
     if (group) {
       group.addParticipant(participantName);
     } else {
@@ -85,7 +71,7 @@ class SecretSantaManager {
 function updateParticipantList(groupName: string) {
   const participantList = document.getElementById("participantList");
   const group = manager.groups.get(groupName);
-
+  
   if (participantList && group) {
     participantList.innerHTML = "";
     group.getParticipants().forEach(participant => {
@@ -99,7 +85,7 @@ function updateParticipantList(groupName: string) {
 function displayAssignments(groupName: string){
   const assignmentsResult = document.getElementById("assignmentsResult");
   const assignments = manager.getGroupAssignment(groupName);
-
+  
   if(assignmentsResult){
     assignmentsResult.innerHTML="";
     assignments.forEach(assignment => {
@@ -114,7 +100,6 @@ const manager = new SecretSantaManager();
 document.getElementById("addParticipantButton")?.addEventListener("click", () => {
   const groupName = document.getElementById("groupUsername")?.value;
   const participantName = document.getElementById("participantName")?.value;
-
   if(groupName && participantName){
     manager.createGroup(groupName);
     manager.addParticipantToGroup(groupName, participantName);
@@ -131,11 +116,9 @@ document.getElementById("drawButton")?.addEventListener("click", () => {
 });
 
 // const manager = new SecretSantaManager;
-
 // manager.createGroup("Grupa1");
 // manager.addParticipantToGroup("Grupa1", "Szymon")
 // manager.addParticipantToGroup("Grupa1", "Antek")
 // //...
 // manager.drawForGroup("Grupa1")
-
 // console.log(`Wyniki dla: Grupa1`, manager.getGroupAssignment("Grupa1"))
